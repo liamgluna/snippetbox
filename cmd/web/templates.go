ab0868cf7f9f2be2a45d3fdc2392492751e8fa23
liamgluna <liamgluna@gmail.com>
2023-08-21 23:40:44 +0800

    template cache

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,40 @@
 package main
 
-import "github.com/liamgluna/snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/liamgluna/snippetbox/internal/models"
+)
 
 type templateData struct {
 	Snippet *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func netTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		
+		files := []string{
+			"./ui/html/base.tmpl.html",
+			"./ui/html/partials/nav.tmpl.html",
+			page,
+		}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
