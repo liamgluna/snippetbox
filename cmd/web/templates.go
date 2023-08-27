cda542f7cd523b88a622f0b9cb1588aad0799857
liamgluna <liamgluna@gmail.com>
2023-08-28 01:26:27 +0800

    .

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -38,7 +38,7 @@ func netTemplateCache() (map[string]*template.Template, error) {
 			return nil, err
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*tmpl.html")
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
