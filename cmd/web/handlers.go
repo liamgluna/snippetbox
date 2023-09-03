938dccd9eee92ae803dceb5b8047d3d2aad868f8
liamgluna <liamgluna@gmail.com>
2023-09-03 23:59:43 +0800

    sessions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -67,7 +67,7 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	form := &snippetCreateForm{}
-	
+
 	err := app.decodePostForm(r, form)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
@@ -91,6 +91,8 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.serverError(w, err)
 		return
 	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
